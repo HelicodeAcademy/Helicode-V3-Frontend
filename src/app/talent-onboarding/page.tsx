65b63ebb420567db97f7186f5ef087239492a1b9
Ayanfeoluwa Noah Akindele <akindeleayanfeoluwa@gmail.com>
2026-03-15 04:26:35 +0100

    UI update: designed talent onboarding page

---
+++ src/app/talent-onboarding/page.tsx
@@ -0,0 +1,46 @@
+import { TalentOnboardingForm } from '@/components/team/talent-onboarding-form';
+import Image from 'next/image';
+import { Toaster } from 'react-hot-toast';
+
+export default function TalentOnboardingPage() {
+  return (
+    <div className="h-screen flex items-stretch md:flex-row flex-col">
+      {/* Left sidebar with logo */}
+      <div className="w-full lg:basis-2/5 flex flex-col justify-start items-center">
+        <div className="relative w-full h-full overflow-hidden">
+          {/* Illustrator wrapper with requested background, rounding and padding */}
+          <div className="relative w-full h-full">
+            {/* Logo positioned inside the illustrator on the left */}
+            <div className="absolute left-6 top-9 z-10">
+              <Image
+                src="/signup/logo.svg"
+                alt="Helicode Logo"
+                width={110}
+                height={24}
+              />
+            </div>
+
+            <div className="relative w-full h-full">
+              <Image
+                src="/signup/Onboarding-Illustration.svg"
+                alt="Illustrator"
+                fill
+                className="object-cover"
+                priority
+              />
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* Right content area */}
+      <div className="w-full lg:basis-3/5 flex flex-col px-6 lg:px-12 py-8 md:py-12">
+        {/* Form */}
+        <div className="flex items-center justify-center h-screen">
+          <TalentOnboardingForm />
+        </div>
+      </div>
+
+      <Toaster position="top-right" />
+    </div>
+  );
+}
