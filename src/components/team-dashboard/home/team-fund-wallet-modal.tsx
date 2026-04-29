fa65c5e9de9a4f00ac445c9c26a02ef9ce8018ad
Marvelousy20 <dev@passiontreecareservices.co.uk>
2026-04-29 18:46:58 +0100

    fixing issues

--- src/components/team-dashboard/home/team-fund-wallet-modal.tsx
+++ src/components/team-dashboard/home/team-fund-wallet-modal.tsx
@@ -49,34 +49,6 @@ export function TeamFundWalletModal({
             />
             <span className="text-[#000000] font-medium">Fund with crypto</span>
           </button>
-
-          {/* Fund with Bank Transfer */}
-          <button
-            onClick={onSelectCard}
-            className="w-full flex cursor-pointer items-center gap-2 p-4 border border-[#E4E7EC] rounded-[6px] hover:bg-gray-50 transition-colors text-left bg-[#F9FAFB]"
-          >
-            <Image src="/wallet/bank.svg" alt="bank" width={24} height={24} />
-            <span className="text-[#000000] font-medium">
-              Fund with bank transfer
-            </span>
-          </button>
-
-          <button
-            onClick={onSelectCard}
-            className="w-full cursor-not-allowed flex items-center gap-2 p-4 border border-[#E4E7EC] rounded-[6px] hover:bg-gray-50 transition-colors text-left bg-[#F9FAFB]"
-            disabled
-          >
-            <Image
-              src="/wallet/credit-card.svg"
-              alt="bank"
-              width={24}
-              height={24}
-            />
-            <span className="text-[#000000] font-medium">Fund with card</span>{" "}
-            <span className="text-[#0052FF] font-medium text-xxs bg-[#E3ECFF] px-2 py-1 rounded-full ml-3">
-              (coming soon)
-            </span>
-          </button>
         </div>
       </DialogContent>
     </Dialog>
