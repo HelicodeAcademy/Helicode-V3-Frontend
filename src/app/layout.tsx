b8cec96affca07be75dd1247bf65e7de298429b9
Marvelousy20 <dev@passiontreecareservices.co.uk>
2026-09-02 14:18:29 +0100

    added google tags

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -84,6 +84,37 @@ export default function RootLayout({
             gtag('config', 'G-5RKZ24R4XG');
           `}
         </Script>
+
+        <Script id="linkedin-partner-id" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "9622338";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+        <Script id="linkedin-insight" strategy="afterInteractive">
+          {`
+            (function(l) {
+              if (!l){window.lintrk = function(a,b){window.lintrk.q.push([a,b])};
+              window.lintrk.q=[]}
+              var s = document.getElementsByTagName("script")[0];
+              var b = document.createElement("script");
+              b.type = "text/javascript";b.async = true;
+              b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+              s.parentNode.insertBefore(b, s);
+            })(window.lintrk);
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=9622338&fmt=gif"
+          />
+        </noscript>
+
         {children}
       </body>
     </html>
